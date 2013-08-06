dacc07add4f6c8ecc7a54b15f348082304bbe8cb
DiegoLopesLima <contact.diego.lima@gmail.com>
2013-08-06 09:27:09 -0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,60 @@ module.exports = function(grunt) {
 
 	var
 
-		pack = grunt.file.readJSON('package.json');
+		pack = grunt.file.readJSON('package.json'),
 
-	grunt.config.init({});
+		data = grunt.file.readJSON(pack.name + '.jquery.json'),
+
+		uglify = {};
+
+	uglify[data.name + '.min.js'] = data.name + '.js';
+
+	grunt.config.init({
+		uglify : {
+			dist : {
+				files : uglify,
+				options : {
+					banner : '/*! ' + data.title + ' | http://plugins.jquery.com/' + data.name + '/' + data.version + ' */\n;'
+				}
+			}
+		},
+		qunit : {
+			dist : {
+				options : {
+					urls : [data.name + '.test.html']
+				}
+			}
+		},
+		jshint : {
+			dist : {
+				src : [pack.name + '.js'],
+				options : {
+					camelcase : true,
+					immed : true,
+					indent : true,
+					latedef : true,
+					newcap : true,
+					noarg : true,
+					noempty : true,
+					quotmark : true,
+					undef : true,
+					unused : true,
+					strict : true,
+					trailing : true,
+					browser: true,
+					globals : {
+						jQuery : true
+					}
+				}
+			}
+		},
+		watch : {
+			dist : {
+				files : [data.name + '.js'],
+				tasks : ['uglify:dist', 'qunit:dist', 'jshint:dist']
+			}
+		}
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
@@ -16,5 +67,5 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['uglify:dist', 'jshint:dist', 'qunit:dist']);
+	grunt.registerTask('default', ['uglify:dist', 'qunit:dist', 'jshint:dist']);
 };
